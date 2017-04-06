268612e7eefe39be6dce6ebb7c43676daf0cb02d
cylus <cylus5745@gmail.com>
2017-04-06 13:03:25 +0530

    choosing 10000 data ponits randomly by running hospital_svm.py from the rows.csv file

--- hospital_svm.py
+++ hospital_svm.py
@@ -1,28 +1,44 @@
-import numpy as np
+#import numpy as np
+import random
 
-data_file=open("hospital_data.csv","r")
+data_file=open("rows.csv","r")
 number=0
 data=[]
 for line in data_file:
     #print(line)
     split_line=line.split(',')
-    if '"Not Available"' in split_line:
+    """if '"Not Available"' in split_line:
         continue
-    else:
-        area=split_line[0]
-        area=area[1:-1]
-        infection=split_line[2]
-        infection=infection[1:-1]
-        score=split_line[3]
-        score=score[1:-1]
-        temp_data = [area,infection,score]
-        data.append(temp_data)
-        number=number+1
-"""print("number of lines: ",number)
-for i in range(0,len(data)):
-    print(data[i])
-for i in data:
-    print i"""
-s=" "
-for i in data:
-    print(s.join(i))
+    else:"""
+    area=split_line[1]
+    #area=area[1:-1]
+    infection=split_line[2]
+    #infection=infection[1:-1]
+    score=split_line[3]
+    #score=score[1:-1]
+    temp_data = [area,infection,score]
+    data.append(temp_data)
+    number=number+1
+    #print(line)
+random.shuffle(data)
+rand_data=data[0:10000]
+# while(len(rand_data)<10000):
+#     temp=random.choice(data)
+#     if temp in rand_data:
+#         pass
+#     else:
+#         rand_data.append(temp)
+#
+# for i in rand_data:
+#     print(i)
+"""examples=0
+key=[0,1]
+for line in data:
+    if random.choice(key) == 1 and examples<10000:
+        examples=examples+1
+        print(line)
+        rand_data.append(line)
+print(len(rand_data))
+"""
+for i in rand_data:
+    print(i)
